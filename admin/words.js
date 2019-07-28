1b4d677419f15478d9244b0dfaec245a02195db6
Scrounger <scrounger@gmx.net>
2019-07-28 17:22:36 +0200

    translation added

--- admin/words.js
+++ admin/words.js
@@ -14,29 +14,17 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla linkeddevices",
 		"zh-cn": "linkeddevices的适配器设置"
 	},
-	"notDeleteDeadLinkedObjects": {
-		"en": "Do not delete objects that are no longer linked",
+	"do not delete objects that are no longer linked": {
+		"en": "do not delete objects that are no longer linked",
 		"de": "Nicht mehr verlinkte Objekte nicht löschen",
-		"ru": "option1",
-		"pt": "option1",
-		"nl": "option1",
-		"fr": "option1",
-		"it": "option1",
-		"es": "option1",
-		"pl": "option1",
-		"zh-cn": "option1"
-	},
-	"option2": {
-		"en": "option2",
-		"de": "option2",
-		"ru": "option2",
-		"pt": "option2",
-		"nl": "option2",
-		"fr": "option2",
-		"it": "option2",
-		"es": "option2",
-		"pl": "option2",
-		"zh-cn": "option2"
+		"ru": "не удаляйте объекты, которые больше не связаны",
+		"pt": "não excluir objetos que não estão mais vinculados",
+		"nl": "verwijder geen objecten die niet langer gekoppeld zijn",
+		"fr": "ne pas supprimer les objets qui ne sont plus liés",
+		"it": "non eliminare oggetti che non sono più collegati",
+		"es": "No borre objetos que ya no están vinculados",
+		"pl": "nie usuwaj obiektów, które nie są już połączone",
+		"zh-cn": "不要删除不再链接的对象"
 	},
 	"composite id of linked object": {
 		"en": "composite id of linked object",
